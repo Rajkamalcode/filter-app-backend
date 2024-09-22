fffa1cbe1555c14eae13b6024903866a5ab5cc45
Rajkamalcode <srajkamal717@gmail.com>
2024-09-22 19:28:23 +0530

    newfolder

--- src/routes/bfhlRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bfhlController = require('../controllers/bfhlController');
-
-router.get('/', bfhlController.handleGetRequest);
-router.post('/', bfhlController.handlePostRequest);
-
-module.exports = router;
